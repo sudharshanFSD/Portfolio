cfae7d2047047c50b12758fddc805c9df655ea78
Muthu kumar <muthukumar24081999@gmail.com>
2024-10-04 20:34:58 +0530

    first commit

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -2,12 +2,22 @@
 export const ProjectList = [
     {
       img: "/IMS2.png",
-      title: "ProGoods Inventory Management System",
+      title: "ProGoods Inventory Management",
       description:
         `The inventory management system allows users to efficiently manage their inventory.`,
-      tech_stack: ["React.js", "Node.js", "Express.js", "MongoDB"],
+      tech_stack: ["React.js","Bootstrap", "Node.js", "Express.js", "MongoDB"],
       github_url: "https://github.com/muthukumar24/Capstone-Project-FE.git",
       demo_url: "https://progoods-inventory-management.netlify.app/",
+    },
+    {
+      img: "/e-commerce.png",
+      title: "Forever E-Commerce",
+      description:
+        `Forever E-Commerce is a dynamic online shopping platform that offers a wide range of products,
+         categorized for Men, Women, and Kids.`,
+      tech_stack: ["React.js", "Tailwind CSS","Node.js", "Express.js", "MongoDB"],
+      github_url: "https://github.com/muthukumar24/forever-e-commerce-FE.git",
+      demo_url: "https://forever-ecommerce-application.netlify.app/",
     },
     {
       img: "/TODOAPPIMG.png",
@@ -122,18 +132,18 @@ experience for customers looking to purchase high-quality audio products.`,
       img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
       name: "Bootstrap",
     },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
-      name: "Material UI",
-    },
+    // {
+    //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    //   name: "Material UI",
+    // },
     // {
     //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
     //   name: "Figma",
     // },
-    // {
-    //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
-    //   name: "TailwindCSS",
-    // },
+    {
+      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+      name: "TailwindCSS",
+    },
     {
       img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
       name: "VS Code",
