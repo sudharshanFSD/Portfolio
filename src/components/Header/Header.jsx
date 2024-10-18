c33ee09fc28d97b8b63ab58619ba4ae7957808a7
sudharshanFSD <tsrssudharshan@gmail.com>
2024-10-19 02:37:25 +0530

    resume fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://drive.google.com/file/d/1Tyjfx5Q7ig1hM_yCSN3P3KeOPQTeJtEs/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1Tyjfx5Q7ig1hM_yCSN3P3KeOPQTeJtEs/view"
             target="_blank"
             rel="noopener noreferrer"
           >
